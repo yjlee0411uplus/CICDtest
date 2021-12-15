194aa69460b0c51cc5b6fbc874f4c2f1b31e7113
eugene <yjlee0411uplus@gmail.com>
2021-12-15 10:09:19 +0900

    ORIGIN

--- add2.java
+++ add2.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
 
-public class Add2 
+public class add2 
 {
 	public static void main(String[] args)
 	{
-		System.out.print("hello world");
-#		Scanner input = new Scanner(System.in);
+//		System.out.print("hello world");
+		Scanner input = new Scanner(System.in);
 
-#		int x;
-#		int y;
-#		int z;
-#		int sum;
+		int x;
+		int y;
+//		int z;
+		int sum;
 
-#		System.out.print("1st Int: ");
-#		x = input.nextInt();
+		System.out.print("1st Int: ");
+		x = input.nextInt();
 
-#		System.out.print("2nd Int: ");
-#		y = input.nextInt();
+		System.out.print("2nd Int: ");
+		y = input.nextInt();
 
-#		System.out.print("3rd Int: ");
-#		z = input.nextInt();
+//		System.out.print("3rd Int: ");
+//		z = input.nextInt();
 
-#		sum = x + y;
-#		sum = x + y + z;
+		sum = x + y;
+//		sum = x + y + z;
 
-#		System.out.println(sum);
-#		input.close();
+		System.out.println(sum);
+		input.close();
 	}
 }
