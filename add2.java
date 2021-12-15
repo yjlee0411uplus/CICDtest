bbc6c1fc5e38b384dd929680b85d4e2053e7affb
eugene <yjlee0411uplus@gmail.com>
2021-12-15 10:17:59 +0900

    SECOND

--- add2.java
+++ add2.java
@@ -4,7 +4,7 @@ public class add2
 {
 	public static void main(String[] args)
 	{
-		System.out.print("hello world");
+//		System.out.print("hello world");
 		Scanner input = new Scanner(System.in);
 
 		int x;
