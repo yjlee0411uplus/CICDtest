a5481125dbdc35e76fe26e557e2645d9b3a863d3
eugene <yjlee0411uplus@gmail.com>
2021-12-15 09:06:59 +0900

    Hello World

--- add2.java
+++ add2.java
@@ -4,26 +4,27 @@ public class Add2
 {
 	public static void main(String[] args)
 	{
-		Scanner input = new Scanner(System.in);
+		System.out.print("hello world");
+#		Scanner input = new Scanner(System.in);
 
-		int x;
-		int y;
+#		int x;
+#		int y;
 #		int z;
-		int sum;
+#		int sum;
 
-		System.out.print("1st Int: ");
-		x = input.nextInt();
+#		System.out.print("1st Int: ");
+#		x = input.nextInt();
 
-		System.out.print("2nd Int: ");
-		y = input.nextInt();
+#		System.out.print("2nd Int: ");
+#		y = input.nextInt();
 
 #		System.out.print("3rd Int: ");
 #		z = input.nextInt();
 
-		sum = x + y;
+#		sum = x + y;
 #		sum = x + y + z;
 
-		System.out.println(sum);
-		input.close();
+#		System.out.println(sum);
+#		input.close();
 	}
 }
