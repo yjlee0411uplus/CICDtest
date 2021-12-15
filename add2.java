8da05690bdde96a744180b31f26ebd41ea4dc264
eugene <yjlee0411uplus@gmail.com>
2021-12-15 11:12:26 +0900

    No input anymore

--- add2.java
+++ add2.java
@@ -5,26 +5,28 @@ public class add2
 	public static void main(String[] args)
 	{
 //		System.out.print("hello world");
-		Scanner input = new Scanner(System.in);
+//		Scanner input = new Scanner(System.in);
 
 		int x;
 		int y;
 		int z;
 		int sum;
 
-		System.out.print("1st Int: ");
-		x = input.nextInt();
+		System.out.print("x: 10");
+//		x = input.nextInt();
 
-		System.out.print("2nd Int: ");
-		y = input.nextInt();
+		System.out.print("y: 20");
+//		y = input.nextInt();
 
-		System.out.print("3rd Int: ");
-		z = input.nextInt();
+		System.out.print("z: 30");
+//		z = input.nextInt();
 
 //		sum = x + y;
 		sum = x + y + z;
 
+		System.out.println(" ");
 		System.out.println(sum);
-		input.close();
+		System.out.pringln(" ");
+//		input.close();
 	}
 }
