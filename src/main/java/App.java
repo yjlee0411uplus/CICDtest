f835deee3b21fbce1234f3bd841230bd9bc7b479
eugene <yjlee0411uplus@gmail.com>
2022-01-18 08:45:08 +0900

    Removed comment

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,16 +7,16 @@ public class App {
 	int z = 30;
 	int sum;
 
-//	System.out.println("x: 10");
-//	System.out.println("y: 20");
-//	System.out.println("z: 30");
+	System.out.println("x: 10");
+	System.out.println("y: 20");
+	System.out.println("z: 30");
 
 	sum = x + y + z;
 
-//	System.out.println(" ");
-//	System.out.println("sum: "+sum);
-//	System.out.println(" ");
-//
+	System.out.println(" ");
+	System.out.println("sum: "+sum);
+	System.out.println(" ");
+
 	return sum;
     }
 
